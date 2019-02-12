e5c5842ea4bc03dd688f1745e682905f780f93a3
Arseny Osipov <aosipov@arago.de>
2019-02-12 17:00:24 +0100

    simplify

--- stats.py
+++ stats.py
@@ -12,6 +12,8 @@ def system_call(command):
 
 parser = argparse.ArgumentParser(description='creates statistics for the release file in the "results" dir')
 parser.add_argument("-f", "--release-file", dest="file", help="path to release file", default="release_6.0.2.json")
+parser.add_argument("-d", "--directory", dest="directory", help="results directory", default="results")
+
 args = parser.parse_args()
 release_file = args.file
 
@@ -30,17 +32,17 @@ months.append(datetime.date.today())
 with open(release_file, "r") as data_file:
     data = json.load(data_file)
 
-directory = "results"
+directory = args.directory
 if not os.path.exists(directory):
     os.makedirs(directory)
 
-for i in data["components"]:
+for i in data:
     o = open(directory + "/" + i["name"] + ".csv", "w")
     print (i["name"])
     o.write('''Month,Commiters,Commits\n''')
     #clone
     if not os.path.exists(i["name"]):
-        system_call("git clone --branch %s %s %s" % (i["tag"], i["source"], i["name"]))
+        system_call("git clone --branch %s %s %s" % (i["branch"], i["source"], i["name"]))
     #gather stats
     for m in range(0, (len(months)-1)):
         o.write("%s - %s," % (months[m], months[m+1]))
@@ -55,23 +57,23 @@ for i in data["components"]:
         c.write(cloc)
 
 
-for i in data["dependencies"]:
-    if "skip" not in i.keys():
-        print (i["name"])
-        o = open(directory + "/" + i["name"] + ".csv", "w")
-        o.write('''Month,Commiters,Commits\n''')
-        #clone
-        if not os.path.exists(i["name"]):
-            system_call("git clone --branch %s %s %s" % (i["tag"], i["source"], i["name"]))
-        #gather stats
-        for m in range(0, (len(months)-1)):
-            o.write("%s - %s," % (months[m], months[m+1]))
-            committers = system_call('cd %s && git shortlog --after="%s" --before="%s" -sn | wc -l | tr -d " |\n"' % (i["name"], months[m], months[m+1]))
-            o.write("%s," % committers)
-            commits = system_call('cd %s && git rev-list --count HEAD --after="%s" --before="%s"'% (i["name"], months[m], months[m+1]))
-            o.write("%s" % commits)
-        o.close()
-        #cloc
-        with open(directory + "/" + "%s_cloc.txt" % i["name"], "w") as c:
-            cloc = system_call("cloc %s --quiet" % i["name"])
-            c.write(cloc)
+# for i in data["dependencies"]:
+#     if "skip" not in i.keys():
+#         print (i["name"])
+#         o = open(directory + "/" + i["name"] + ".csv", "w")
+#         o.write('''Month,Commiters,Commits\n''')
+#         #clone
+#         if not os.path.exists(i["name"]):
+#             system_call("git clone --branch %s %s %s" % (i["branch"], i["source"], i["name"]))
+#         #gather stats
+#         for m in range(0, (len(months)-1)):
+#             o.write("%s - %s," % (months[m], months[m+1]))
+#             committers = system_call('cd %s && git shortlog --after="%s" --before="%s" -sn | wc -l | tr -d " |\n"' % (i["name"], months[m], months[m+1]))
+#             o.write("%s," % committers)
+#             commits = system_call('cd %s && git rev-list --count HEAD --after="%s" --before="%s"'% (i["name"], months[m], months[m+1]))
+#             o.write("%s" % commits)
+#         o.close()
+#         #cloc
+#         with open(directory + "/" + "%s_cloc.txt" % i["name"], "w") as c:
+#             cloc = system_call("cloc %s --quiet" % i["name"])
+#             c.write(cloc)
